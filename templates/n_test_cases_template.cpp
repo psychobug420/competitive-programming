624eefc3b75ca7dc9d3669d2104463a0357ca1ea
Antoni Filonau <antoni.filonau@gmail.com>
2025-12-27 14:51:16 +0100

    tools: Added basic puzzle template with CMake support

---
+++ templates/n_test_cases_template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+void do_test_case()
+{
+}
+
+int main()
+{
+    size_t tests_num{};
+    std::cin >> tests_num;
+
+    for (size_t i = 0; i < tests_num; ++i)
+    {
+        do_test_case();
+    }
+
+    return 0;
+}
