7d996a1931a3eaba5d5206a79b06db8803f15afa
Antoni Filonau <antoni.filonau@gmail.com>
2026-01-06 19:58:53 +0100

    practice: Timus 1293 Eniya

---
+++ practice/timus/1293_eniya.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    uint64_t n{}, a{}, b{};
+
+    std::cin >> n >> a >> b;
+    std::cout << 2 * n * a * b << std::endl;
+
+    return 0;
+}
