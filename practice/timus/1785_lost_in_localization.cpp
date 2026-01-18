d5e47922464383ecc950874b99f04aae2127b564
Antoni Filonau <antoni.filonau@gmail.com>
2026-01-18 22:18:41 +0100

    practice: Timus 1785 Lost In Localization

---
+++ practice/timus/1785_lost_in_localization.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    const std::map<uint32_t, std::string> dict{
+        {4, "few"},
+        {9, "several"},
+        {19, "pack"},
+        {49, "lots"},
+        {99, "horde"},
+        {249, "throng"},
+        {499, "swarm"},
+        {999, "zounds"},
+        {2000, "legion"},
+    };
+
+    uint32_t n{};
+    std::cin >> n;
+
+    auto it = dict.lower_bound(n);
+
+    std::cout << it->second << std::endl;
+
+    return 0;
+}
