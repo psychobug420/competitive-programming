55b86e7fb38e26c57c9afd1b2508e96d106084b7
Antoni Filonau <antoni.filonau@gmail.com>
2026-01-28 23:01:28 +0100

    practice: Timuse 1820 Ural Steaks

---
+++ practice/timus/1820_ural_steaks.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int64_t n{}, k{};
+    std::cin >> n >> k;
+
+    std::cout << std::max(2ll, (2 * n + k - 1) / k) << std::endl;
+
+    return 0;
+}
